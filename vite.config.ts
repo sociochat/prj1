dbf509f1d8882383c35880e15a71f7ffe35d2b9f
sociochat <70502693+sociochat@users.noreply.github.com>
2026-01-05 01:43:20 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'sociochat/prj1',
+  base: '/prj1/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
